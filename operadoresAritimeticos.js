498bef20ca89fe4f607b4b72e657445110392a44
Vinicios Neves <vinicios.neves@mac>
2025-05-30 19:26:36 +0100

    video 5.1

---
+++ operadoresAritimeticos.js
@@ -0,0 +1,28 @@
+let numero1 = 10;
+let numero2 = 3;
+
+let soma = numero1 + numero2;
+console.log("Soma:", soma);
+
+let subtracao = numero1 - numero2;
+console.log("Subtração:", subtracao);
+
+let multiplicacao = numero1 * numero2;
+console.log("Multiplicação:", multiplicacao);
+
+let divisao = numero1 / numero2;
+console.log("Divisão:", divisao);
+
+let resto = numero1 % numero2;
+console.log("Resto da divisão:", resto);
+
+let potencia = numero1 ** numero2;
+console.log("Potência:", potencia);
+
+// Incremento e decremento
+let contador = 5;
+contador++;
+console.log("Incremento:", contador);
+
+contador--;
+console.log("Decremento:", contador);
