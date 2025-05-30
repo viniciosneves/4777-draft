f07cd748ebe0494edd62b29a06b51c0432cf705e
Vinicios Neves <vinicios.neves@mac>
2025-05-30 19:05:36 +0100

    video 4.2

--- variaveis.js
+++ variaveis.js
@@ -1,9 +1,17 @@
-idade = 36;
-console.log("Idade:", idade);
-
-
-// isso aqui é um comentário!
-// fiz aniversário
+var idade = 36;
+console.log("Idade com var:", idade);
 
 idade = 37;
-console.log("Fiz aniversário!", idade);
+console.log("Fiz aniversário! Idade agora é:", idade);
+
+let altura = 1.75;
+console.log("Altura:", altura);
+
+altura = 1.76;
+console.log("Cresci! Altura agora é:", altura);
+
+const nome = "Vinicios";
+
+// nome = 'Marcos'
+
+console.log("Nome:", nome);
