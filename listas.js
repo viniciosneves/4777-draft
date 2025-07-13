973ca02174396a1c49181efcf415ebe9a2a8dd50
Vinicios Neves <vinicios.neves@mac>
2025-07-13 11:43:35 +0100

    video 9.1

---
+++ listas.js
@@ -0,0 +1,36 @@
+// arrays.js
+// Rodar com: node arrays.js
+
+console.log('Trabalhando com Arrays em JavaScript\n');
+
+// Criando um array
+const frutas = ['maçã', 'banana', 'uva', 'morango'];
+
+console.log('Array original:', frutas);
+
+// Acessando elementos (lembre: índice começa em 0)
+console.log('Primeira fruta:', frutas[0]);
+console.log('Última fruta:', frutas[frutas.length - 1]);
+
+// Verificando tamanho
+console.log('Total de frutas:', frutas.length);
+
+console.log('\n--- Métodos úteis ---');
+
+// push: adiciona no final
+frutas.push('melancia');
+console.log('Depois do push:', frutas);
+
+// pop: remove do final
+const ultima = frutas.pop();
+console.log('Depois do pop:', frutas);
+console.log('Fruta removida:', ultima);
+
+// unshift: adiciona no início
+frutas.unshift('laranja');
+console.log('Depois do unshift:', frutas);
+
+// shift: remove do início
+const primeira = frutas.shift();
+console.log('Depois do shift:', frutas);
+console.log('Fruta removida:', primeira);
