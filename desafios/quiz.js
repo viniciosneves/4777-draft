ea4ea1ffa7a3f14460a95d60dc10f1930026ebc8
Vinicios Neves <vinicios.neves@mac>
2025-07-13 11:22:29 +0100

    video 8.6

--- desafios/quiz.js
+++ desafios/quiz.js
@@ -1,5 +1,27 @@
-let numero1 = 7;
-let numero2 = "3";
-let resultado = numero1 + numero2;
+// quiz.js
+// Rodar com: node quiz.js
 
-console.log(resultado);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Bem-vindo(a) ao Quiz de JavaScript!');
+console.log('Responda com a letra correta: a, b ou c\n');
+
+// Primeira pergunta
+rl.question('1) Qual palavra usamos para criar uma função?\n(a) define\n(b) function\n(c) create\n> ', (resposta1) => {
+  let acertos = 0;
+
+  if (resposta1 === 'b') {
+    acertos++;
+  }
+
+  // TODO: adicionar a segunda pergunta
+
+  // TODO: adicionar a terceira pergunta
+
+  // TODO: mostrar o resultado final e encerrar o quiz com rl.close()
+});
